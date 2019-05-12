6edb5292a4b103395da5b19fcdf34f7f9ee34086
Tracy Ngo <tngo1206@gmail.com>
2019-05-12 00:55:17 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -9,7 +9,7 @@ class Artist < ActiveRecord::Base
 
   def song_count
     #return the number of songs associated with the artist
-    self.songs.length
+    self.songs.count
   end
 
   def genre_count
